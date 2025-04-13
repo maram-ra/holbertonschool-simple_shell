8354f010bdd8ce914336404708bf92be3530c222
maram-ra <maramraddad33@gmail.com>
2025-04-13 15:27:31 +0000

    Add

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include "shell.h"
 
+#define MAX_ARGS 64
+
 /**
- * main - Basic shell loop
+ * only_spaces - Checks if a line contains only whitespace
+ * @s: The input string
+ *
+ * Return: 1 if only spaces, 0 otherwise
+ */
+int only_spaces(char *s)
+{
+	while (*s)
+	{
+		if (*s != ' ' && *s != '\t' && *s != '\n')
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
+/**
+ * main - Basic shell loop (handles whitespace and arguments)
+ *
  * Return: 0 on success
  */
 int main(void)
@@ -9,7 +29,9 @@ int main(void)
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
-	char *args[2];
+	char *args[MAX_ARGS];
+	int i;
+	pid_t pid;
 
 	while (1)
 	{
@@ -23,18 +45,33 @@ int main(void)
 		if (line[read - 1] == '\n')
 			line[read - 1] = '\0';
 
-		args[0] = line;
-		args[1] = NULL;
+		if (only_spaces(line))
+			continue;
 
-		if (fork() == 0)
+		/* Tokenize the line */
+		i = 0;
+		args[i] = strtok(line, " ");
+		while (args[i] != NULL && i < MAX_ARGS - 1)
+		{
+			i++;
+			args[i] = strtok(NULL, " ");
+		}
+		args[i] = NULL;
+
+		if (args[0] == NULL)
+			continue;
+
+		pid = fork();
+		if (pid == 0)
 		{
 			execve(args[0], args, environ);
 			perror("execve");
-			exit(1);
+			exit(EXIT_FAILURE);
 		}
 		else
 			wait(NULL);
 	}
+
 	free(line);
 	return (0);
 }
