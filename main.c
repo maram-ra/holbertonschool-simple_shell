0b0af96d74e396ee56a37d562bb0b17aba2716b9
maram-ra <maramraddad33@gmail.com>
2025-04-13 15:05:09 +0000

    Add

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * main - Entry point of the shell
+ * @argc: argument count (unused)
+ * @argv: argument vector
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	return (0);
+}
