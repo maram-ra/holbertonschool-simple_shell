fecb779290a5186144b9fadffef9019aa8582046
maram-ra <maramraddad33@gmail.com>
2025-04-13 15:19:59 +0000

    Add

--- main.c
+++ main.c
@@ -1,16 +1,41 @@
 #include "shell.h"
 
 /**
- * main - Entry point of the shell
- * @argc: argument count (unused)
- * @argv: argument vector
- *
- * Return: Always 0 (success)
+ * main - Basic shell loop
+ * Return: 0 on success
  */
-int main(int argc, char **argv)
+int main(void)
 {
-	(void)argc;
-	(void)argv;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char *args[2];
 
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", 2);
+
+		read = getline(&line, &len, stdin);
+		if (read == -1) /* Ctrl+D */
+			break;
+
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
+		args[0] = line;
+		args[1] = NULL;
+
+		if (fork() == 0)
+		{
+			execve(args[0], args, environ);
+			perror("execve");
+			exit(1);
+		}
+		else
+			wait(NULL);
+	}
+	free(line);
 	return (0);
 }
+
