67082a2a1e65a5b65ae7205e79bd90ac8e27d206
Hessafa <10685@holbertonstudents.com>
2025-04-15 16:06:13 +0000

    declare find_command function

--- shell.h
+++ shell.h
@@ -1,13 +1,27 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/* ==== STANDARD LIBRARIES ==== */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 
+/* ==== MACROS ==== */
+#define MAX_ARGS 64
+
+/* ==== GLOBAL VARIABLES ==== */
 extern char **environ;
 
-#endif /* SHELL_H */
+/* ==== FUNCTION PROTOTYPES ==== */
 
+/* main.c */
+int only_spaces(char *s);
+
+/* path.c */
+char *find_command(char *cmd);
+
+#endif /* SHELL_H */
