a0692617d52c50569d4721fc3c2cc24568a802ff
Hessafa <10685@holbertonstudents.com>
2025-04-15 16:28:39 +0000

    update main.c to handle command resolution with PATH

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 #define MAX_ARGS 64
 
 /**
@@ -20,59 +19,92 @@ int only_spaces(char *s)
 }
 
 /**
- * main - Basic shell loop (handles whitespace and arguments)
+ * prompt_user - Displays the prompt and reads the user input
  *
- * Return: 0 on success
+ * Return: The input line or NULL if there's an error
  */
-int main(void)
+char *prompt_user(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
-	char *args[MAX_ARGS];
-	int i;
-	pid_t pid;
 
-	while (1)
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+
+	read = getline(&line, &len, stdin);
+	if (read == -1) /* Ctrl+D or EOF */
 	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "$ ", 2);
-
-		read = getline(&line, &len, stdin);
-		if (read == -1) /* Ctrl+D */
-			break;
-
-		if (line[read - 1] == '\n')
-			line[read - 1] = '\0';
-
-		if (only_spaces(line))
-			continue;
-
-		/* Tokenize the line */
-		i = 0;
-		args[i] = strtok(line, " ");
-		while (args[i] != NULL && i < MAX_ARGS - 1)
-		{
-			i++;
-			args[i] = strtok(NULL, " ");
-		}
-		args[i] = NULL;
-
-		if (args[0] == NULL)
-			continue;
-
-		pid = fork();
-		if (pid == 0)
-		{
-			execve(args[0], args, environ);
-			perror("execve");
-			exit(EXIT_FAILURE);
-		}
-		else
-			wait(NULL);
+		free(line);
+		return (NULL);
 	}
 
-	free(line);
-	return (0);
+	if (line[read - 1] == '\n')
+		line[read - 1] = '\0';
+
+	return (line);
 }
 
+/**
+ * tokenize_input - Tokenizes the input line into arguments
+ * @line: The input line
+ * @args: The arguments array
+ *
+ * Return: The number of arguments
+ */
+int tokenize_input(char *line, char *args[])
+{
+	int i = 0;
+
+	args[i] = strtok(line, " ");
+	while (args[i] != NULL && i < MAX_ARGS - 1)
+	{
+		i++;
+		args[i] = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+
+	return (i);
+}
+
+/**
+ * execute_command - Handles the forking and execution of commands
+ * @args: The arguments array
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int execute_command(char *args[])
+{
+	pid_t pid;
+	char *command_path;
+
+	command_path = find_command(args[0]);
+	if (command_path == NULL)
+	{
+		fprintf(stderr, "%s: command not found\n", args[0]);
+		return (-1);
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		/* In child: execute the command */
+		execve(command_path, args, environ);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid > 0)
+	{
+		/* In parent: wait for child to finish */
+		wait(NULL);
+		free(command_path);
+	}
+	else
+	{
+		/* Handle fork failure */
+		perror("fork");
+		free(command_path);
+		return (-1);
+	}
+
+	return
