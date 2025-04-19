7992eb55520c74152b50ae948e8d40ef6a70cfa0
Hessafa <10685@holbertonstudents.com>
2025-04-19 09:01:42 +0000

    Improved file organization and ensured compliance with Betty

--- utils.c
+++ utils.c
@@ -1,76 +1,6 @@
 #include "shell.h"
 
 /**
- * only_spaces - Checks if a line contains only whitespace
- * @s: The input string
- *
- * Return: 1 if only spaces, 0 otherwise
- */
-int only_spaces(char *s)
-{
-	while (*s)
-	{
-		if (*s != ' ' && *s != '\t' && *s != '\n')
-			return (0);
-		s++;
-	}
-	return (1);
-}
-
-/**
- * read_line - Reads a line from standard input
- *
- * Return: Pointer to the read line (must be freed), or NULL on EOF/empty line
- */
-char *read_line(void)
-{
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-
-	read = getline(&line, &len, stdin);
-	if (read == -1)
-	{
-		free(line);
-		return (NULL);
-	}
-
-	if (line[read - 1] == '\n')
-		line[read - 1] = '\0';
-
-	if (only_spaces(line))
-	{
-		free(line);
-		return (NULL);
-	}
-
-	return (line);
-}
-
-/**
- * parse_arguments - Tokenizes a command line into arguments
- * @line: Input command line
- * @args: Array to store the arguments
- */
-void parse_arguments(char *line, char **args)
-{
-	int i = 0;
-	char *token;
-
-	while (*line == ' ' || *line == '\t')
-		line++;
-
-	token = tokenize_input(line);
-	while (token != NULL && i < MAX_ARGS - 1)
-	{
-		args[i] = token;
-		i++;
-		token = tokenize_input(NULL);
-	}
-	args[i] = NULL;
-}
-
-/**
  * _strlen - Calculates the length of a string (like strlen)
  * @s: The input string
  *
@@ -80,6 +10,7 @@ int _strlen(const char *s)
 {
 	int len = 0;
 
+	/* Loop until we reach the null terminator */
 	while (s[len])
 		len++;
 
@@ -98,14 +29,42 @@ int _strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t i;
 
+	/* Compare character by character until n or difference is found */
 	for (i = 0; i < n && s1[i] && s2[i]; i++)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 	}
 
+	/* If we reached here but one string ended early */
 	if (i < n)
 		return (s1[i] - s2[i]);
 
-	return (0);
+	return (0); /* Strings are equal up to n */
+}
+
+/**
+ * _strdup - Duplicates a string (like the standard strdup)
+ * @str: The input string to duplicate
+ *
+ * Return: A pointer to the newly allocated copy of the string,
+ *         or NULL if memory allocation fails
+ */
+char *_strdup(const char *str)
+{
+	char *dup;
+	int len = _strlen(str);
+	int i;
+
+	/* Allocate memory for the new string (+1 for null terminator) */
+	dup = malloc(sizeof(char) * (len + 1));
+	if (!dup)
+		return (NULL);
+
+	/* Copy characters from original string */
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+
+	dup[i] = '\0'; /* Null-terminate the new string */
+	return (dup);
 }
