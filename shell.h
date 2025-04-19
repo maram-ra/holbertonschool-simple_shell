5e046830fee6e3e5d6728bf54c49905f18756665
Hessafa <10685@holbertonstudents.com>
2025-04-19 08:23:44 +0000

    Fixing Whitespace

--- shell.h
+++ shell.h
@@ -1,7 +1,18 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/* ==== STANDARD LIBRARIES ==== */
+/**
+ * shell.h - Header file for a simple UNIX-like shell.
+ *
+ * Description: Contains function prototypes, macros, and global
+ *              variables used throughout the shell program.
+ * Author: Hessah & Maram
+ */
+
+ /* =================================================================== */
+ /*                         STANDARD LIBRARY HEADERS                         */
+ /* =================================================================== */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,30 +21,48 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
-/* ==== MACROS ==== */
-#define MAX_ARGS 64
-#define _GNU_SOURCE
+/* ========================================================================= */
+/*                                 MACROS                                    */
+/* ========================================================================= */
 
-/* ==== GLOBAL VARIABLES ==== */
+#define MAX_ARGS 64 /* Maximum number of arguments supported */
+#define _GNU_SOURCE /* Enable GNU extensions (e.g., getline) */
+
+/* ========================================================================= */
+/*                             GLOBAL VARIABLES                              */
+/* ========================================================================= */
+
 extern char **environ;
 extern int last_status;
 extern char *line;
 
-/* ==== FUNCTION PROTOTYPES ==== */
+/* ========================================================================= */
+/*                          FUNCTION PROTOTYPES                              */
+/* ========================================================================= */
+
+/* Entry point */
 int main(void);
+
+/* Input and Parsing */
 int only_spaces(char *s);
 char *read_line(void);
 void parse_arguments(char *line, char **args);
+char *tokenize_input(char *line);
+
+/* Command Execution */
 char *find_command_path(char *command);
 void execute_command(char *command_path, char **args);
 void handle_command(char **args);
+
+/* Built-in Commands */
+int check_builtin(char **args);
+void print_env(void);
+void free_args_and_exit(void);
+
+/* String Utilities */
 char *get_env_value(const char *name);
 char *_strdup(const char *str);
 int _strlen(const char *s);
 int _strncmp(const char *s1, const char *s2, size_t n);
-int check_builtin(char **args);
-void free_args_and_exit(void);
-void print_env(void);
-char *tokenize_input(char *line);
 
-#endif 
+#endif /* SHELL_H */
