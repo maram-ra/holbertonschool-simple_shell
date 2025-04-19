0dfbfa8215fe77e97fb6f6d785fd1cdc2ed91f6a
Hessafa <10685@holbertonstudents.com>
2025-04-19 12:16:13 +0000

    Fixing the code

--- find_command_path.c
+++ find_command_path.c
@@ -1,4 +1,8 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
 /**
  * find_command_path - Searches for a command in PATH
@@ -16,31 +20,31 @@ char *find_command_path(char *command)
 	{
 		if (stat(command, &st) == 0 && (st.st_mode & S_IXUSR))
 			return (strdup(command));
-		return (NULL);
+		return (NULL);  /* Command not found */
 	}
 
 	/* If PATH is missing or empty, we can't proceed */
 	path_env = get_env_value("PATH");
-	if (!path_env || *path_env == '\0')
+	if (!path_env || *path_env == '\0')  /* If PATH is empty */
 		return (NULL);
 
 	path_copy = strdup(path_env);
 	if (!path_copy)
 		return (NULL);
 
+	/* Search for the command in each directory listed in PATH */
 	dir = strtok(path_copy, ":");
 	while (dir)
 	{
-		snprintf(full_path, sizeof(full_path),
-			"%s/%s", dir, command);
+		snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
 		if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
 		{
 			free(path_copy);
-			return (strdup(full_path));
+			return (strdup(full_path));  /* Return the full path if found */
 		}
-		dir = strtok(NULL, ":");
+		dir = strtok(NULL, ":");  /* Move to the next directory in PATH */
 	}
 
-	free(path_copy);
-	return (NULL);
+	free(path_copy);  /* Free the copied PATH */
+	return (NULL);  /* Command not found in PATH */
 }
