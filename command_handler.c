ac4f321de8d4b3121ad6af9cfae97afdaeb53ed3
Hessafa <10685@holbertonstudents.com>
2025-04-19 12:31:15 +0000

    fix the code to align with the task

--- command_handler.c
+++ command_handler.c
@@ -1,25 +1,26 @@
+#include "shell.h"
+
 /**
- * handle_command - Executes the command if found
+ * handle_command - Handles execution of command
  * @args: Argument vector
  */
 void handle_command(char **args)
 {
 	char *command_path;
 
-	if (!args || !args[0])
+	if (!args || !args[0])  /* If no command, do nothing */
 		return;
 
-	command_path = find_command_path(args[0]);
-	if (command_path)
+	command_path = find_command_path(args[0]);  /* Find the command path */
+	if (command_path)  /* If found */
 	{
-		execute_command(command_path, args);
-		free(command_path);
+		execute_command(command_path, args);  /* Execute the command */
+		free(command_path);  /* Free the command path after execution */
 	}
-	else
+	else  /* Command not found */
 	{
-		write(STDERR_FILENO, "./hsh: 1: ", 10);
-		write(STDERR_FILENO, args[0], _strlen(args[0]));
-		write(STDERR_FILENO, ": not found\n", 13);
-		last_status = 127; /* Update status to indicate error */
+		write(STDERR_FILENO, args[0], _strlen(args[0]));  /* Print command name */
+		write(STDERR_FILENO, ": command not found\n", 20);  /* Print error message */
+		last_status = 127;  /* Set the last status to indicate failure */
 	}
 }
