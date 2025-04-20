423b4e5fafefb1ce19f14a36b117dfe88c622263
Hessafa:git config --global user.email 10685@holbertonstudents.com  git config --global user.name Hessafa <10685@holbertonstudents.com>
2025-04-20 07:42:30 +0000

    Fxing Betty for all Files

--- shell.h
+++ shell.h
@@ -10,7 +10,6 @@
  */
 
 /* ================= STANDARD LIBRARY HEADERS ================ */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -18,45 +17,43 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+
 /* =========================== MACROS ======================== */
-
-#define MAX_ARGS 64 /* Maximum number of arguments supported */
-#define _GNU_SOURCE /* Enable GNU extensions (e.g., getline) */
+#define MAX_ARGS 64        /* Maximum number of arguments supported */
 #define BUFSIZE 1024
-/* ===================== GLOBAL VARIABLES ==================== */
+#define _GNU_SOURCE        /* Enable GNU extensions (e.g., getline) */
 
+/* ===================== GLOBAL VARIABLES ==================== */
 extern char **environ;
 extern int last_status;
 extern char *line;
 
 /* ================== FUNCTION PROTOTYPES ==================== */
 
-/* input_utils.c */
+/* --------------- Input/Command Processing ---------------- */
 int only_spaces(char *s);
 char *read_line(void);
 void parse_arguments(char *line, char **args);
-
-/* tokenizer.c */
 char *tokenize_input(char *line);
 
-/* string_utils.c */
+/* --------------------- String Utilities ------------------- */
 int _strlen(const char *s);
 int _strncmp(const char *s1, const char *s2, size_t n);
 char *_strdup(const char *str);
 char *get_env_value(const char *name);
 
-/* command execution */
+/* ------------------- Command Execution ------------------- */
 char *find_command_path(char *command);
 void execute_command(char *command_path, char **args);
 void handle_command(char **args);
 void main_loop(int interactive);
 
-/* builtins */
+/* ---------------------- Built-ins ------------------------ */
 int check_builtin(char **args);
 void print_env(void);
 void free_args_and_exit(void);
 
-/* Error Handling */
+/* -------------------- Error Handling --------------------- */
 void print_error(char *cmd, char *msg);
 
 #endif /* SHELL_H */
